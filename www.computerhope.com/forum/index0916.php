da41fe34062a9ae3d32f4281a9de8e339c48f9f4
nadimakram99 <akramnadim1999@mail.com>
2020-04-23 21:30:31 +0530

    computerhopby nadim
    this is a demo

---
+++ www.computerhope.com/forum/index0916.php
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="ISO-8859-1"?>
+<rss version="2.0" xmlns:mon="http://www.microsoft.com/schemas/rss/monitoring/2007" xml:lang="en-US">
+	<channel>
+		<title>Computer Hope - Recent Posts</title>
+		<link>https://www.computerhope.com/forum/index.php?action=recent</link>
+		<description><![CDATA[Live information from Computer Hope]]></description>
+		<item>
+			<title>Computer Hope - Recent Posts</title>
+			<link>https://www.computerhope.com/forum/index.php?action=recent</link>
+			<description><![CDATA[
+				
+	<link rel="stylesheet" href="https://www.computerhope.com/forum/Themes/default/css/wireless.css" type="text/css" />
+				
+	<div style="width: 100%; height: 100%; border: 1px solid black; padding: 0; margin: 0 0 0 0; font: 100.01%/100% Verdana, Helvetica, sans-serif;">
+		<div style="background-color: #080436; color: #ffffff; padding: 4px;">
+			Recent Posts
+		</div>
+		<div style="background-color: #F6F6F6; font-size: 90%; padding: 2px;">
+			<strong><a href="https://www.computerhope.com/forum/index.php?topic=176031.msg998492#msg998492">Topic: Need help for ransomware virus  </a></strong> by <a href="https://www.computerhope.com/forum/index.php?action=profile;u=191074">hearthot2545</a>
+		</div>
+		<div style="background-color: #ECEDF3; font-size: 90%; padding: 2px;">
+			<strong><a href="https://www.computerhope.com/forum/index.php?topic=176023.msg998491#msg998491">Re: win 7 to win 10</a></strong> by <a href="https://www.computerhope.com/forum/index.php?action=profile;u=140339">strollin</a>
+		</div>
+		<div style="background-color: #F6F6F6; font-size: 90%; padding: 2px;">
+			<strong><a href="https://www.computerhope.com/forum/index.php?topic=176023.msg998490#msg998490">Re: win 7 to win 10</a></strong> by <a href="https://www.computerhope.com/forum/index.php?action=profile;u=191068">Pejorative</a>
+		</div>
+		<div style="background-color: #ECEDF3; font-size: 90%; padding: 2px;">
+			<strong><a href="https://www.computerhope.com/forum/index.php?topic=176030.msg998489#msg998489">MOVED: win 7 to win 10</a></strong> by <a href="https://www.computerhope.com/forum/index.php?action=profile;u=72918">Allan</a>
+		</div>
+		<div style="background-color: #F6F6F6; font-size: 90%; padding: 2px;">
+			<strong><a href="https://www.computerhope.com/forum/index.php?topic=176023.msg998488#msg998488">Re: win 7 to win 10</a></strong> by <a href="https://www.computerhope.com/forum/index.php?action=profile;u=72918">Allan</a>
+		</div>
+	</div>
+	<div style="width: 100%; height: 100%; border: 0; padding: 0; margin: 0 0 0 0; font: 100.01%/100% Verdana, Helvetica, sans-serif;">
+		<div style="font-size: xx-small;" class="righttext">
+			<a href="https://www.computerhope.com/forum/index.php?action=login">Login</a>
+		</div>
+	</div>
+				
+			]]></description>
+		</item>
+	</channel>
+</rss>
---
+++ www.computerhope.com/forum/indexb599.php
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="ISO-8859-1"?>
+<rss version="0.92" xml:lang="en-US">
+	<channel>
+		<title>Computer Hope</title>
+		<link>https://www.computerhope.com/forum/index.php</link>
+		<description><![CDATA[Live information from Computer Hope]]></description>
+		<item>
+			<title>Topic: Need help for ransomware virus  </title>
+			<link>https://www.computerhope.com/forum/index.php/topic,176031.msg998492.html#msg998492</link>
+			<description><![CDATA[I have a huge problem, my dads computer got infected by CTB-locker a ransomware but he made the mistake to restore his files to the same date his computer got infected, I removed the virus but, <a href="https://5e48fb2a253d8.site123.me/" class="bbc_link" target="_blank"><span style="color: #e7eaef;" class="bbc_color">[siz...</span></a>]]></description>
+			<category><![CDATA[Computer viruses and spyware]]></category>
+			<comments>https://www.computerhope.com/forum/index.php?action=post;topic=176031.0</comments>
+			<pubDate>Wed, 19 Feb 2020 14:35:43 GMT</pubDate>
+			<guid>https://www.computerhope.com/forum/index.php/topic,176031.msg998492.html#msg998492</guid>
+		</item>
+		<item>
+			<title>Re: win 7 to win 10</title>
+			<link>https://www.computerhope.com/forum/index.php/topic,176023.msg998491.html#msg998491</link>
+			<description><![CDATA[If you do an upgrade, you can choose whether or not to preserve all your installed programs and data.&nbsp; If you remove Win 7 first (you&#39;ll need to format your disk to do that), you&#39;ll start with an empty disk and after installing the OS, will need to rei...]]></description>
+			<category><![CDATA[Windows 10]]></category>
+			<comments>https://www.computerhope.com/forum/index.php?action=post;topic=176023.0</comments>
+			<pubDate>Wed, 19 Feb 2020 14:27:32 GMT</pubDate>
+			<guid>https://www.computerhope.com/forum/index.php/topic,176023.msg998491.html#msg998491</guid>
+		</item>
+		<item>
+			<title>Re: win 7 to win 10</title>
+			<link>https://www.computerhope.com/forum/index.php/topic,176023.msg998490.html#msg998490</link>
+			<description><![CDATA[If you bought your windows 7 then you should be able to upgrade to windows 10 free.]]></description>
+			<category><![CDATA[Windows 10]]></category>
+			<comments>https://www.computerhope.com/forum/index.php?action=post;topic=176023.0</comments>
+			<pubDate>Wed, 19 Feb 2020 13:29:30 GMT</pubDate>
+			<guid>https://www.computerhope.com/forum/index.php/topic,176023.msg998490.html#msg998490</guid>
+		</item>
+		<item>
+			<title>MOVED: win 7 to win 10</title>
+			<link>https://www.computerhope.com/forum/index.php/topic,176030.msg998489.html#msg998489</link>
+			<description>
+<![CDATA[This topic has been moved to <a href="https://www.computerhope.com/forum/index.php?board=59.0" class="bbc_link" target="_blank">Windows 10</a>.<br /><br /><a href="https://www.computerhope.com/forum/index.php?topic=176023.0" class="bbc_link">https://www.computerhope.com/forum/index.php?topic=176023.0</a>]]>
+			</description>
+			<category><![CDATA[Hardware]]></category>
+			<comments>https://www.computerhope.com/forum/index.php?action=post;topic=176030.0</comments>
+			<pubDate>Wed, 19 Feb 2020 12:28:28 GMT</pubDate>
+			<guid>https://www.computerhope.com/forum/index.php/topic,176030.msg998489.html#msg998489</guid>
+		</item>
+		<item>
+			<title>Re: win 7 to win 10</title>
+			<link>https://www.computerhope.com/forum/index.php/topic,176023.msg998488.html#msg998488</link>
+			<description><![CDATA[Why would this be a hardware question. Clearly it should be in the Windows forum. Please use some common sense when posting. I&#39;ll move it this time.]]></description>
+			<category><![CDATA[Windows 10]]></category>
+			<comments>https://www.computerhope.com/forum/index.php?action=post;topic=176023.0</comments>
+			<pubDate>Wed, 19 Feb 2020 12:28:15 GMT</pubDate>
+			<guid>https://www.computerhope.com/forum/index.php/topic,176023.msg998488.html#msg998488</guid>
+		</item>
+	</channel>
+</rss>
